f1121741b578faddaa4abf002e609c179f6c4e70
洪嘉勇 <tjhjyisland@gmail.com>
2017-01-03 16:50:31 +0800

    还差一个首页的买入

--- public/controller/mycenterCtrl.js
+++ public/controller/mycenterCtrl.js
@@ -17,6 +17,7 @@ app.controller('mycenterCtrl', ['$scope', '$http', 'constService', function ($sc
     $scope.method = '登录';
     $scope.userId = '';
     $scope.user;
+    $scope.error='买入';
     this.$onInit = function () {
 
         if ($('#state').text() === 'false'){
@@ -190,6 +191,19 @@ app.controller('mycenterCtrl', ['$scope', '$http', 'constService', function ($sc
         $('#put-in-modal').modal({
             onApprove: function () {
                 console.log($('#put-in').val());
+                $http({
+                    method: 'POST',
+                    url: server + '/up',
+                    params:{
+                        'id': $scope.user.id,
+                        'number': $('#put-in').val()
+                    }
+                }).then( res=>{
+                    console.log(res.data);
+                    $scope.user.left_money = Number($scope.user.left_money) + Number($('#put-in').val());
+                }).catch( err=>{
+                    console.log(err);
+                })
             }
         }).modal('show');
     };
@@ -199,6 +213,19 @@ app.controller('mycenterCtrl', ['$scope', '$http', 'constService', function ($sc
         $('#put-out-modal').modal({
             onApprove: function () {
                 console.log($('#put-out').val());
+                $http({
+                    method: 'POST',
+                    url: server + '/out',
+                    params:{
+                        'id': $scope.user.id,
+                        'number': $('#put-out').val()
+                    }
+                }).then( res=>{
+                    console.log(res.data);
+                    $scope.user.left_money = Number($scope.user.left_money) - Number($('#put-out').val());
+                }).catch( err=>{
+                    console.log(err);
+                })
             }
         }).modal('show');
     };
@@ -280,8 +307,246 @@ app.controller('mycenterCtrl', ['$scope', '$http', 'constService', function ($sc
         }, 500);
     };
 
+    var yesterday_data = {
+        title: {
+            text: '',
+            x: -20 //center
+        },
+        subtitle: {
+            text: '',
+            x: -20
+        },
+        xAxis: {
+            categories: []
+        },
+        yAxis: {
+            title: {
+                text: '元 (￥)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '￥'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: '昨日动态',
+            data: []
+        }]
+    };
+
+
     // 展示数据
-    $scope.showData = function () {
-        alert('test');
-    }
+    $scope.showData = function (future, index) {
+        yesterday_data.title.text = future.future_name;
+        yesterday_data.xAxis.categories = [];
+        yesterday_data.series[0].data = [];
+        $http({
+            method: 'GET',
+            url: server + '/future/yesterday',
+            params:{
+                'f_id': future.future_id,
+                'u_id': $scope.user.id
+            }
+        }).then( res=>{
+            console.log(res.data);
+            yesterday_data.subtitle.text = '涨幅：' + res.data.rate;
+            for (let i = 0; i < res.data.yesterdayListView.length; i++){
+                yesterday_data.xAxis.categories.push(res.data.yesterdayListView[i].index);
+                yesterday_data.series[0].data.push(res.data.yesterdayListView[i].price);
+            }
+
+            Highcharts.chart('chart-' + index, yesterday_data);
+
+        }).catch( err=>{
+            console.log(err);
+        })
+    };
+
+    // 加仓
+    $scope.buyFuture = function (future) {
+        $scope.error = '买入';
+        $('#buy-modal').modal({
+                closable: false
+            }).modal('show');
+        $('#future-id').text(future.future_id);
+    };
+
+    $scope.submitBuy = function () {
+        $.ajax({
+            method: 'POST',
+            cache: false,
+            url: server + '/buy',
+            data: JSON.stringify({
+                'f_Id': $('#future-id').text(),
+                'u_Id': $scope.user.id,
+                'number': $('#buy-number').val(),
+                'price': $('#buy-baojia').val(),
+                'pwd': $('#buy-pwd').val()
+            }),
+            processData: false,
+            contentType: 'application/json'
+        }).done( function(res){
+            console.log(res);
+            $('#buy-modal').modal('hide');
+
+            if (res === 1){
+                mdui.snackbar({
+                    message: '操作成功！'
+                });
+                //刷新用户信息
+                $http({
+                    method: 'POST',
+                    url: server + '/user/profile',
+                    params:{
+                        'id': $scope.userId
+                    }
+                }).then( res=>{
+                    console.log(res.data);
+                    $scope.user.my_future = res.data.my_future;
+                    $scope.user.left_money = res.data.left_money;
+                    $scope.user.amount = res.data.amount;
+                }).catch( err=>{
+                    console.log(err);
+                });
+
+            }else if(res === 2){
+                mdui.snackbar({
+                    message: '您的余额不足，购买失败了'
+                });
+                setTimeout(function () {
+                    $('#buy-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 3){
+                mdui.snackbar({
+                    message: '您的报价低于最低价，购买失败了'
+                });
+                setTimeout(function () {
+                    $('#buy-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 4){
+                mdui.snackbar({
+                    message: '我们的后台挂了，购买失败了'
+                });
+                setTimeout(function () {
+                    $('#buy-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 5){
+                mdui.snackbar({
+                    message: '您的密码错误，购买失败了'
+                });
+                setTimeout(function () {
+                    $('#buy-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }
+        })
+    };
+
+    // 减仓
+    $scope.sellFuture = function (future) {
+        $('#sell-modal').modal({
+            closable: false
+        }).modal('show');
+        $('#future-id').text(future.future_id);
+    };
+
+    $scope.submitSell = function () {
+        $.ajax({
+            method: 'POST',
+            cache: false,
+            url: server + '/sell',
+            data: JSON.stringify({
+                'f_Id': $('#future-id').text(),
+                'u_Id': $scope.user.id,
+                'number': $('#sell-number').val(),
+                'price': $('#sell-baojia').val(),
+                'pwd': $('#sell-pwd').val()
+            }),
+            processData: false,
+            contentType: 'application/json'
+        }).done( function(res){
+            console.log(res);
+            $('#sell-modal').modal('hide');
+
+            if (res === 1){
+                mdui.snackbar({
+                    message: '操作成功！'
+                });
+                //刷新用户信息
+                $http({
+                    method: 'POST',
+                    url: server + '/user/profile',
+                    params:{
+                        'id': $scope.userId
+                    }
+                }).then( res=>{
+                    console.log(res.data);
+                    $scope.user.my_future = res.data.my_future;
+                    $scope.user.left_money = res.data.left_money;
+                    $scope.user.amount = res.data.amount;
+                }).catch( err=>{
+                    console.log(err);
+                });
+
+            }else if(res === 2){
+                mdui.snackbar({
+                    message: '您的仓库存储不足，卖出失败了'
+                });
+                setTimeout(function () {
+                    $('#sell-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 3){
+                mdui.snackbar({
+                    message: '您的报价高于市场价格，卖出失败了'
+                });
+                setTimeout(function () {
+                    $('#sell-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 4){
+                mdui.snackbar({
+                    message: '我们的后台挂了，卖出失败了'
+                });
+                setTimeout(function () {
+                    $('#sell-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }else if(res === 5){
+                mdui.snackbar({
+                    message: '您的密码错误，卖出失败了'
+                });
+                setTimeout(function () {
+                    $('#sell-modal').modal({
+                        closable: false
+                    }).modal('show');
+                }, 2000);
+            }
+        })
+    };
+
+    // 取消
+    $scope.back = function () {
+        $('.ui.modal').modal('hide');
+    };
 }]);
